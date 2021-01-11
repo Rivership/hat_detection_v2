098d119bf35d66f9544cb2e41f1094d86e98aac7
alan zhao <zhaofuyang@hnu.edu.cn>
2021-01-11 20:46:29 +0800

    备份参数等

---
+++ tools/voc_label.py
@@ -0,0 +1,58 @@
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from os import listdir, getcwd
+from os.path import join
+
+#sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+#classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["hat", "person"]
+
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+def convert_annotation(image_id):
+    in_file = open('/home/zhao/2021/hat-detection/hat-detect/dataset/VOC2028/Annotations/%s.xml'%image_id.replace('\n',''))
+    out_file = open('/home/zhao/2021/hat-detection/hat-detect/dataset/VOC2028/labels/%s.txt'%image_id.replace('\n',''), 'w')
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult) == 1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        bb = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+'''
+wd = getcwd()
+
+for year, image_set in sets:
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+'''
+def my_convert():
+    image_ids = open('/home/zhao/2021/hat-detection/hat-detect/dataset/VOC2028/ImageSets/Main/test.txt')
+    list_file = open('/home/zhao/2021/hat-detection/hat-detect/dataset/VOC2028/test.txt', 'w')
+    for image_id in image_ids:
+        list_file.write('data/hat/%s.jpg\n'%image_id.replace('\n',''))
+        convert_annotation(image_id)
+    list_file.close()
+
+my_convert()
